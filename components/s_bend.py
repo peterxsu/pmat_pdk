d7bec6c66e106ef549fa267fde3a15d8e48ef46f
Peter Su <peterxsu@mit.edu>
2017-08-20 01:04:56 -0400

    Modified SBend to better deal with shorter and longer spirals that wouldn't necessarily fit

--- components/s_bend.py
+++ components/s_bend.py
@@ -24,8 +24,8 @@ class SBend:
         self.clad_width = wg_template.clad_width
         self.bend_radius = wg_template.bend_radius
         
-        if delta_y > 2*self.bend_radius:
-            raise ValueError("delta_y greater than 2 bend radii")
+        if abs(delta_y) > 2*self.bend_radius:
+            self.length = 2*self.bend_radius
         else:
             self.length = 2*math.sqrt(self.bend_radius**2-(self.bend_radius-abs(delta_y)/2)**2)
         
@@ -41,7 +41,10 @@ class SBend:
         start = self.start
         length = self.length
         
-        angle = math.acos((br-abs(delta_y)/2)/br)*180/math.pi
+        if abs(delta_y) > 2*br:
+            angle = 90
+        else:
+            angle = math.acos((br-abs(delta_y)/2)/br)*180/math.pi
         
         """ cladding """
         if delta_y > 0:
@@ -54,6 +57,10 @@ class SBend:
         self.cell.add(input_clad_arc)
         self.cell.add(output_clad_arc)
         
+        if abs(delta_y) > 2*br:
+            straight_clad_wg = shapes.Rectangle((start[0]+br-cw/2,start[1]+math.copysign(br,delta_y)), (start[0]+br+cw/2,start[1]+delta_y-math.copysign(br,delta_y)), layer=2)
+            self.cell.add(straight_clad_wg)
+        
         """ s-bend """
         if delta_y > 0:
             input_arc = shapes.Disk((start[0],start[1]+br), br+wg_width/2, inner_radius=br-wg_width/2, initial_angle=270, final_angle=270+angle, layer=1)
@@ -65,6 +72,10 @@ class SBend:
         self.cell.add(input_arc)
         self.cell.add(output_arc)
         
+        if abs(delta_y) > 2*br:
+            straight_wg = shapes.Rectangle((start[0]+br-wg_width/2,start[1]+math.copysign(br,delta_y)), (start[0]+br+wg_width/2,start[1]+delta_y-math.copysign(br,delta_y)), layer=1)
+            self.cell.add(straight_wg)
+        
     def build_ports(self):
         """ Portlist format:  [x_position, y_position, wg_exit_angle] """
         self.portlist["input1"] = [self.start[0], self.start[1], "WEST"]
@@ -72,6 +83,6 @@ class SBend:
 
 if __name__ == "__main__":
     wgt = WaveguideTemplate(bend_radius=100.0, wg_width=2.0, clad_width=20.0)
-    swg = SBend("sb", wgt, delta_y=-10.0)
+    swg = SBend("sb", wgt, delta_y=1000.0)
     swg.cell.show()
     print swg.portlist
